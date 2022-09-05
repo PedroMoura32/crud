8afebe9f009155ced338fa3f109dd415888c3e48
Pedro Moura <henriquepedro3223@protonmail.com>
2022-09-05 16:24:35 -0300

    tentativa de criar a função para checagem de ID

--- src/middleware.js
+++ src/middleware.js
@@ -4,9 +4,25 @@ exports.middlewareCheck = (req, res, next) => {
 
     if(users.length === 0) {
         res.json({
-            "msg": "Ainda não temos cadastro!"
+            "msg": "Ainda não temos usuários!"
         })
     } else {
         next()
     }
 }
+
+// exports.middlewareCheckId = (req, res, next) => {
+//     for(let i = 0; i < users.length; i++) {
+//         req.params.id = +req.params.id
+
+//         if(users[i].id === req.params.id) {
+//             console.log('condição positiva')
+//             next()
+//         } else {
+//             console.log('condição NEGATIVA')
+//             res.json({
+//                 "msg": "não temos este id"
+//             })
+//         }
+//     }
+// }
