906366ec5705aaff0cfac1bae59be543ac7bb199
Pedro Moura <henriquepedro3223@protonmail.com>
2022-09-05 16:28:37 -0300

    Adição da middlewareCheck em outras rotas

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const route = express.Router()
+const { middlewareCheck } = require('./middleware')
 
 const { getAllUsers, getUser, createUser, deleteUser, updateUser } = require('./controllers')
 
-route.get('/users', getAllUsers) // rota para listar todos os geeks
-route.get('/users/:id', getUser)
+route.get('/users', middlewareCheck, getAllUsers) // rota para listar todos os geeks
+route.get('/users/:id', middlewareCheck, getUser)
 route.post('/users', createUser)
-route.delete('/users/:id', deleteUser)
-route.put('/users/:id', updateUser)
+route.delete('/users/:id', middlewareCheck, deleteUser)
+route.put('/users/:id', middlewareCheck, updateUser)
 
 module.exports = route
